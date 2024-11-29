3615652734e6e527f4b996c294aae91beeaa21d2
Amarok <snowlyicewolf@outlook.com>
2024-11-29 13:16:46 +0800

    🐺💻🍍

--- collection/source/apiqqut/collection/package.d
+++ collection/source/apiqqut/collection/package.d
@@ -1,10 +1,11 @@
 module apiqqut.collection;
 
-public:
-import apiqqut.collection.list;
-import apiqqut.collection.map;
-import apiqqut.collection.table;
-import apiqqut.collection.pair;
-import apiqqut.collection.multimap;
-import apiqqut.collection.iterator;
-import apiqqut.collection.conv;
+public {
+    import apiqqut.collection.list;
+    import apiqqut.collection.map;
+    import apiqqut.collection.table;
+    import apiqqut.collection.pair;
+    import apiqqut.collection.multimap;
+    import apiqqut.collection.iterator;
+    import apiqqut.collection.conv;
+}
--- csv/source/apiqqut/csv/csv.d
+++ csv/source/apiqqut/csv/csv.d
@@ -4,120 +4,117 @@ import std.file;
 import std.string;
 import std.conv : to;
 
-alias TargetType = string[][string];
+string encodec(string[][] data) {
+    string datas = "";
 
-void encodec(TargetType data, string filePath, bool lineModeCodec = false) {
-    string csvString(string str) => "\"" ~ str ~ "\"";
-    string[][] dataList = new string[][0];
+    foreach (string[] line; data) {
+        foreach (string str; line) {
+            datas ~= "\"" ~ str ~ "\"" ~ ",";
+        }
+        datas = datas[0 .. -1] ~ "\n";
+    }
+
+    return datas;
+}
+
+string encodecFromRow(string[][string] data) {
+    string[][] datas = new string[][data.length];
     string[] keys = data.keys;
-    foreach (string key; keys) {
-        string[] value = data[key];
-        dataList ~= (new string[0]) ~ key ~ value;
+    for(int i = 0; i < keys.length; i++) {
+        string key = keys[i];
+        datas[i] = key ~ data[key];
     }
 
-    string[] lines = new string[0];
-    int max;
-    foreach (string[] list; dataList) {
-        if (list.length > max) {
-            max = to!int(list.length);
-        }
-    }
-
-    if (lineModeCodec) {
-        foreach (string[] list; dataList) {
-            string line;
-            for (int i = 0; i < max; i++) {
-                if (list.length < i) {
-                    string val = list[i];
-                    line ~= indexOf(val, ",") > 0 ? val : csvString(val);
-                }
-
-                line ~= ",";
-            }
-            lines ~= line[0 .. $ - 1];
-        }
-    } else {
-        for (int i = 0; i < max; i++) {
-            string line;
-            foreach (string[] list; dataList) {
-                if (list.length < i) {
-                    string val = list[i];
-                    line ~= indexOf(val, ",") > 0 ? val : csvString(val);
-                }
-                line ~= ",";
-            }
-            lines ~= line[0 .. $ - 1];
-        }
-    }
-
-    write(filePath, "");
-    foreach(string line; lines) {
-        append(filePath, line ~"\n");
-    }
+    return encodec(datas);
 }
 
-TargetType decodec(string filePath, bool lineModeCodec = false) {
-    string[] texts = splitLines(readText(filePath));
-
-    string[][] csvDatas = new string[][0];
-
-    foreach (string line; texts) {
-        string[] splits = splitCsv(line);
-        csvDatas ~= splits;
-    }
-
-    string[][string] map;
-
-    for (int i = 0; i < csvDatas.length; i++) {
-        string[] dat = csvDatas[i];
-        if (lineModeCodec) {
-            map[dat[0]] = dat[1 .. $];
-            continue;
-        }
-
-        if (i == 0) {
-            foreach (string key; dat) {
-                map[key] = new string[0];
+string encodecFromCol(string[][string] data) {
+    string[][] datas = new string[][1];
+    string[] keys = data.keys;
+    datas[0] = keys;
+    for(int i = 0; i < keys.length; i++) {
+        string key = keys[i];
+        string[] dat = data[key];
+        for(int o = 1; o <= dat.length; o++) {
+            if (o >= datas.length) {
+                datas ~= new string[0];
             }
-            continue;
-        }
+            string[] val = datas[o];
 
-        for (int o = 0; o < dat.length; o++) {
-            map[csvDatas[0][o]] ~= dat[o];
+            while (val.length < i) {
+                val ~= "";
+            }
+
+            val[i] = dat[o];
         }
     }
 
-    return map;
+    return encodec(datas);
 }
 
-private string[] splitCsv(string line) {
-    string[] words = new string[0];
-    bool start;
-    while (line.length > 0) {
-        if (line.startsWith("\"")) {
-            int index = to!int(indexOf(line[1 .. $], '\"'));
-            if (index == -1) {
-                throw new Error("The string has start but no end!");
+string[][] decodec(string datas) {
+    string[] lines = splitLines(datas);
+
+    string[][] data = new string[][lines.length];
+
+    for (int i = 0; i < lines.length; i++) {
+        data[i] = new string[0];
+
+        string line = lines[i];
+        string str = "";
+        for (int o = 0; o < line.length; o++) {
+            char c = line[o];
+
+            switch (c) {
+            case '\"':
+                break;
+            case '\\':
+                str ~= c;
+                str ~= line[++o];
+                break;
+            case ',':
+                data[i] ~= str;
+                str = "";
+                break;
+            default:
+                str ~= c;
+                break;
             }
-
-            words ~= line[1 .. index];
-            if (index + 1 == line.length) {
-                return words;
-            }
-
-            line = line[index + 2 .. $];
-            continue;
         }
-
-        int index = to!int(indexOf(line, ','));
-        if (index == -1) {
-            words ~= line;
-            return words;
-        }
-
-        words ~= line[0 .. index];
-        line = line[index + 1 .. $];
     }
 
-    return words;
+    return data;
+}
+
+string[][string] decodecToRow(string datas) {
+    string[][] csvlist = decodec(datas);
+    string[][string] dataRow;
+    foreach (string[] str; csvlist) {
+        dataRow[str[0]] = str[1 .. $];
+    }
+
+    return dataRow;
+}
+
+string[][string] decodecToCol(string datas) {
+    string[][] csvlist = decodec(datas);
+    string[][string] dataCol;
+
+    string[] keyLine = csvlist[0];
+
+    foreach (string key; keyLine) {
+        dataCol[key] = new string[0];
+    }
+
+    for (int i = 1; i < csvlist.length; i++) {
+        string[] lineOf = csvlist[i];
+        for (int o = 0; o < keyLine.length; o++) {
+            if (o >= lineOf.length) {
+                break;
+            }
+            dataCol[keyLine[o]] ~= lineOf[o];
+        }
+    }
+
+    return dataCol;
 }
