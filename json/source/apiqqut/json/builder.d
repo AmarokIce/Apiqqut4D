177591c461303b5fd4e1f74b5455a7688e599e08
Amarok <snowlyicewolf@outlook.com>
2024-12-02 12:21:10 +0800

    🐺‼️

---
+++ json/source/apiqqut/json/builder.d
@@ -0,0 +1,73 @@
+module apiqqut.json.builder;
+
+
+//! @BETA
+//! In dev.
+
+interface ITask {
+    ITask addTask(ITask task);
+    string toJson();
+    string toJson5();
+}
+
+class MapBuilder : ITask {
+    ITask[] taskList = new ITask[0];
+
+    override ITask addTask(ITask task) {
+        taskList ~= task;
+        return this;
+    }
+
+    override string toJson() {
+        string str;
+        return str;
+    }
+
+    override string toJson5() {
+        string str;
+        return str;
+    }
+
+    ITask add(string key, string value) {
+        return this.addTask(new PairTask(key, value));
+    }
+
+    private class PairTask : ITask {
+        private string key, value;
+        this(string key, string value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        override ITask addTask(ITask task) {
+            return this;
+        }
+
+        override string toJson() {
+            return key ~ ": " ~ "value";
+        }
+
+        override string toJson5() {
+            return this.toJson;
+        }
+    }
+}
+
+class ArrayBuilder : ITask {
+    ITask[] taskList = new ITask[0];
+
+    override ITask addTask(ITask task) {
+        taskList ~= task;
+        return this;
+    }
+
+    override string toJson() {
+        string str;
+        return str;
+    }
+
+    override string toJson5() {
+        string str;
+        return str;
+    }
+}
