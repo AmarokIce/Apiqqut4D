d65e850566dc3820a1bf2359bb3e38dabf8ab979
Amarok <snowlyicewolf@outlook.com>
2025-03-07 20:49:33 +0800

    🐺📰

--- json/source/apiqqut/json/node.d
+++ json/source/apiqqut/json/node.d
@@ -1,35 +1,137 @@
 module apiqqut.json.node;
 
-import apiqqut.json.codec;
+import std.string;
+import std.conv : to;
 
-class JsonNode {
-    const string baseData;
+/**
+* The base of node, hold the raw data.
+*/
+class JsonNode : JsonElement {
+    private NodeType type = NodeType.UNKNOW;
+    private const string raw;
 
-    this(const string base) {
-        this.baseData = base;
+    this(string raw) {
+        this.raw = raw;
     }
 
-    public T get(T)(Codec!T codec) {
-        return codec.encode(baseData);
+    // TODO
+    public int getAsInt() {
+        import std.regex : matchAll;
+
+        string newStr = "";
+        string tempStr = replace(raw, "_", "");
+        tempStr = split(tempStr, ".")[0];
+
+        string[] zero = split(replace(tempStr, "E", "e"), "e");
+        tempStr = zero[0];
+        auto temp = matchAll(tempStr, "\\d+").front;
+        foreach (string s; temp) {
+            newStr ~= s;
+        }
+
+        if (zero.length > 1) {
+            int zeroCount = to!int(zero[1]);
+
+            for (int i = 0; i < zeroCount; i++) {
+                newStr ~= "0";
+            }
+        }
+
+        return to!int(tempStr);
     }
 
-    public string getString() {
-        return StringCodec.INSTANCE.decode(baseData);
+    public double getAsDouble();
+    public string getAsString();
+    public bool getAsBoolean();
+
+    override void deduce();
+
+    override NodeType getType() {
+        return this.type;
     }
 
-    public int getInteger() {
-        return IntegerCodec.INSTANCE.decode(baseData);
+    override string getRawData() {
+        return this.raw;
+    }
+}
+
+class JsonObject : JsonElement {
+    JsonNode[string] datas;
+
+    override NodeType getType() {
+        return NodeType.JSON_OBJECT;
     }
 
-    public double getDouble() {
-        return DoubleCodec.INSTANCE.decode(baseData);
+    override string getRawData() {
+        string str = "{";
+        foreach (string key; datas.keys) {
+            str ~= ",\n    ";
+            str ~= key ~ ":" ~ datas[key].getRawData;
+        }
+
+        str ~= "\n}";
+        return str;
     }
 
-    public JsonNode[] getArray() {
-        return ArrayCodec.INSTANCE.decode(baseData);
+    override void deduce();
+}
+
+class JsonArray : JsonElement {
+    JsonNode[] nodes;
+
+    JsonNode get(int index) {
+        return nodes[index];
     }
 
-    public JsonNode[string] getMap() {
-        return MapCodec.INSTANCE.decode(baseData);
+    override NodeType getType() {
+        return NodeType.JSON_ARRAY;
     }
+
+    override string getRawData() {
+        string str = "[";
+        foreach (JsonNode key; nodes) {
+            str ~= ",\n    ";
+            str ~= key.getRawData;
+        }
+
+        str ~= "\n]";
+        return str;
+    }
+
+    override void deduce();
+}
+
+abstract class JsonElement {
+    abstract NodeType getType();
+    abstract string getRawData();
+    abstract void deduce();
+
+    JsonObject getAsObject() {
+        if (this.getType() != NodeType.JSON_OBJECT) {
+            throw new Exception("This element not an object!");
+        }
+
+        return cast(JsonObject) this;
+    }
+
+    JsonArray getAsArray() {
+        if (this.getType() != NodeType.JSON_ARRAY) {
+            throw new Exception("This element not an array!");
+        }
+
+        return cast(JsonArray) this;
+    }
+}
+
+enum NodeType {
+    JSON_OBJECT,
+    JSON_ARRAY,
+
+    JSON_STRING,
+    JSON_INTEGER,
+    JSON_DOUBLE,
+    JSON_BOOLEAN,
+
+    // The default type.
+    UNKNOW
 }
